8c7f6fbbeb2c4b06144a3724dc0de3797cda034e
chkm64 <chkm64.goma@gmail.com>
2022-02-09 13:14:03 -0500

    Deploy

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const routerApi = require('./routes');
 const { logErrors, errorHandler, boomHandler } = require('./middlewares/error.handler');
 
 const app = express();
-const port = 3000;
+const port = proccess.env.ṔORT || 3000;
 
 app.use(express.json());
 
 const whitelist = ['http://localhost:8080', 'https://myapp.com'];
 const options = {
   origin: (origin, callback) => {
-    if (whitelist.includes(origin)){
+    if (whitelist.includes(origin) || !origin){
       callback(null, true);
     } else {
       callback(new Error('No permitido'));
